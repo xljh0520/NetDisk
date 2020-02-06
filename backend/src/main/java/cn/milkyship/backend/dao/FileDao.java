38fd7808d484beeafccb214300b5df8c34df29c2
milkyboat <1214208054@qq.com>
2020-02-06 21:52:04 +0800

    updata file system

--- backend/src/main/java/cn/milkyship/backend/dao/FileDao.java
+++ backend/src/main/java/cn/milkyship/backend/dao/FileDao.java
@@ -1,18 +1,33 @@
 package cn.milkyship.backend.dao;
 
+import cn.milkyship.backend.model.fileSystem.FSObject;
 import cn.milkyship.backend.model.fileSystem.NetDirectory;
+import cn.milkyship.backend.model.fileSystem.NetFile;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
 @Component
 public interface FileDao {
 
-	public boolean isDir();
+	public boolean isDir(int FSObjId);
 	
-	public void mkdir(NetDirectory netDirectory);
+	public int add(NetDirectory dir);
+	
+	public int add(NetFile file);
 	
 	public void del(int FileId);
 	
-	// TODO : 完善几个DAO
+	public FSObject find(int FSObjId);
+	
+	public NetDirectory findDir(int DirId);
+	
+	public NetFile findFile(int FileId);
+	
+	public List<NetDirectory> findChildrenDir(int FSObjectId);
+	
+	public List<NetFile> findChildrenFile(int FSObjectId);
+	
 }
--- backend/src/main/java/cn/milkyship/backend/service/FileManageService.java
+++ backend/src/main/java/cn/milkyship/backend/service/FileManageService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,26 +18,84 @@ public class FileManageService {
 	@Autowired
 	private FileDao fileDao;
 	
-	public void mkdir(){
-	
+	public int mkdir(int parentId, String fileName){
+		if(!fileDao.isDir(parentId)) {return -1;}
+		
+		NetDirectory parent = (NetDirectory)fileDao.findDir(parentId);
+		
+		NetDirectory netDirectory = new NetDirectory();
+		
+		netDirectory.setFileName(fileName);
+		netDirectory.setParent(parentId);
+		netDirectory.setOwner(parent.getOwner());
+		netDirectory.setFilePath(parent.getFilePath() + parent.getFileName() + '/');
+		
+		return fileDao.add(netDirectory);
 	}
 	
-	public void findChild(List<FSObject> children){
-	
+	public List<FSObject> findChild( int ManipulatedFileId){
+		List<FSObject> children = new ArrayList<>();
+		if(fileDao.isDir(ManipulatedFileId)){
+			List<NetDirectory> childrenDir = fileDao.findChildrenDir(ManipulatedFileId);
+			if(childrenDir != null)
+				children.addAll(childrenDir);
+			List<NetFile> childrenFile = fileDao.findChildrenFile(ManipulatedFileId);
+			if(childrenFile != null)
+				children.addAll(childrenFile);
+		}
+		else {
+			children.add(fileDao.findFile(ManipulatedFileId));
+		}
+		return children;
 	}
 	
-	public void cut(int ManipulatedFileId, int targetDirId){
-		copy(ManipulatedFileId, targetDirId);
+	public int cut(int ManipulatedFileId, int targetDirId){
+		if(!fileDao.isDir(targetDirId)){return -1;}
+		int re = copy(ManipulatedFileId, targetDirId);
 		del(ManipulatedFileId);
+		return re;
 	}
 	
-	public void copy(int ManipulatedFileId, int targetDirId){
-	
+	/***
+	 * @Title: copy
+	 * @Description: 同一用户下的文件复制
+	 * @param ManipulatedFileId: 被操作的文件id
+	 * @param targetDirId: 目标文件夹id
+	 * @return void
+	 * @author  徐云凯
+	 * @Datetime  2020/2/6 14:39
+	 */
+	public int copy(int ManipulatedFileId, int targetDirId){
+		if(!fileDao.isDir(targetDirId)){return -1;}
+		
+		int ownerId = fileDao.findDir(targetDirId).getOwner();
+		
+		if(fileDao.isDir(ManipulatedFileId)){
+			String ManipulatedDirName = fileDao.findDir(ManipulatedFileId).getFileName();
+			int re = mkdir(targetDirId, ManipulatedDirName);
+			
+			List<NetFile> childrenFile = fileDao.findChildrenFile(ManipulatedFileId);
+			for(NetFile child:childrenFile){
+				//copy(chil);
+				//TODO:
+			}
+			List<NetDirectory> childrenDir = fileDao.findChildrenDir(ManipulatedFileId);
+			for(NetDirectory child:childrenDir){
+				mkdir(targetDirId, child.getFileName());
+				//TODO:
+			}
+			return re;
+		}
+		else{
+			NetFile file = fileDao.findFile(ManipulatedFileId);
+			file.setParent(targetDirId);
+			file.setOwner(ownerId);
+			return fileDao.add(file);
+		}
 	}
 
 	public void del(int ManipulatedFileId){
-		List<FSObject> children = null;
-		findChild(children);
+		List<FSObject> children = findChild(ManipulatedFileId);
 		for(FSObject child:children){
 			if(child instanceof NetFile){
 				fileDao.del(child.getId());
