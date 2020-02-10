152c3137677dea9146f2eddc94ba44bbd4e1cff1
milkyboat <1214208054@qq.com>
2020-02-10 19:20:33 +0800

    rename to Group.java

--- backend/src/main/java/cn/milkyship/backend/model/user/Group.java
+++ backend/src/main/java/cn/milkyship/backend/model/user/Group.java
@@ -2,19 +2,19 @@ package cn.milkyship.backend.model.user;
 
 /*
  * <p>项目名称: backend </p>
- * <p>文件名称: group </p>
+ * <p>文件名称: Group </p>
  * <p>描述: 群组实体类 </p>
  * <p>创建时间: 2020-1-12 </p>
  * <a href="mail to: xuyunkai@mail.nankai.edu.cn">徐云凯</a>
  * @author 徐云凯 FJH
  * @version v1.0
  */
-public class group extends User {
+public class Group extends User {
     private int numMembers;
     private int numFiles;
     private String groupName;
 
-    public group() {
+    public Group() {
     }
 
     public int getNumMembers() {
