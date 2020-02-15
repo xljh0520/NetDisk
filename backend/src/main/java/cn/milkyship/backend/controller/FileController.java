86019f734db1f7ae42a2054b38262d2c09744942
milkyboat <1214208054@qq.com>
2020-02-15 17:13:06 +0800

    add fileController Test

--- backend/src/main/java/cn/milkyship/backend/controller/FileController.java
+++ backend/src/main/java/cn/milkyship/backend/controller/FileController.java
@@ -1,9 +1,15 @@
 package cn.milkyship.backend.controller;
 
+import cn.milkyship.backend.model.fileSystem.FSObject;
 import cn.milkyship.backend.service.FileIOService;
 import cn.milkyship.backend.service.FileManageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * <p>项目名称: netdisk </p>
@@ -15,7 +21,8 @@ import org.springframework.stereotype.Controller;
  * @version v1.0
  */
 
-@Controller
+@RestController
+@RequestMapping (value="/file")     // 通过这里配置使下面的映射都在/users下
 public class FileController {
 
 	@Autowired
@@ -23,6 +30,60 @@ public class FileController {
 	@Autowired
 	private FileManageService fileManageService;
 	
+	@RequestMapping(value="/test/{id}", method= RequestMethod.GET)
+	public List<FSObject> test(@PathVariable String id) {
+		FSObject fsObject = new FSObject();
+		fsObject.setId("id123");
+		fsObject.setFileName("abcTstFile.txt");
+		fsObject.setOwner("owner123");
+		fsObject.setParent("parent123");
+		fsObject.setFilePath("/root/data/");
+		List<FSObject> list = new ArrayList<>();
+		list.add(fsObject);
+		return list;
+	}
 	
-
+	@RequestMapping(value = "ls/{id}", method = RequestMethod.GET)
+	public List<FSObject> ls(@PathVariable String id) {
+		// 处理"/files/{uuid}}"的GET请求，用来读取文件目录
+		return fileManageService.findChild(id);
+	}
+	
+//	@RequestMapping(value="/", method=RequestMethod.POST)
+//	public String postUser(@ModelAttribute User user) {
+//		// 处理"/users/"的POST请求，用来创建User
+//		// 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+//		users.put(user.getId(), user);
+//		return "success";
+//	}
+//
+//	@RequestMapping(value="/download/{id}", method=RequestMethod.GET)
+//	public MultipartFile getUser(@PathVariable String id) {
+//		// 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
+//		// url中的id可通过@PathVariable绑定到函数的参数中
+//		File file = fileIOService.download(id);
+//
+//		if(!file.exists())
+//			return null;
+//
+//
+//	}
+//
+//	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+//	public String putUser(@PathVariable Long id, @ModelAttribute User user) {
+//		// 处理"/users/{id}"的PUT请求，用来更新User信息
+//		User u = users.get(id);
+//		u.setName(user.getName());
+//		u.setAge(user.getAge());
+//		users.put(id, u);
+//		return "success";
+//	}
+//
+//	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+//	public String deleteUser(@PathVariable Long id) {
+//		// 处理"/users/{id}"的DELETE请求，用来删除User
+//		users.remove(id);
+//		return "success";
+//	}
+	
 }
---
+++ backend/src/test/java/cn/milkyship/backend/BackendFileApiTest.java
@@ -0,0 +1,54 @@
+package cn.milkyship.backend;
+
+import cn.milkyship.backend.controller.FileController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/*
+ * <p>项目名称: netdisk </p>
+ * <p>文件名称: BackendFileApiTest </p>
+ * <p>描述: 测试后台api接口可用性 </p>
+ * <p>创建时间: 2020/2/15 </p>
+ * <a href="mail to: xuyunkai@mail.nankai.edu.cn">徐云凯</a>
+ * @author 徐云凯
+ * @version v1.0
+ */
+
+@RunWith (SpringJUnit4ClassRunner.class)
+@SpringBootTest (classes = MockServletContext.class)
+@WebAppConfiguration
+public class BackendFileApiTest {
+	
+	private MockMvc mvc;
+	
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new FileController()).build();
+	}
+	
+	@Test
+	public void testFileController() throws Exception  {
+		// 测试FileController
+		RequestBuilder request = null;
+		
+		// 1、get查一下某目录下的file列表，应该为空
+		request = get("/file/test/123");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"id\":\"id123\",\"fileName\":\"abcTstFile.txt\",\"owner\":\"owner123\",\"parent\":\"parent123\",\"filePath\":\"/root/data/\"}]")));
+		
+	}
+	
+}
